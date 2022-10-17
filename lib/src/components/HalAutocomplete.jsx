6b861f959ef7ed6aa8e8beadef5fca0a4a4b27c9
marcialfps <m.parrilla@dxc.com>
2022-10-17 16:54:35 +0200

    Autocomplete filtering delegated to API call

--- lib/src/components/HalAutocomplete.jsx
+++ lib/src/components/HalAutocomplete.jsx
@@ -2,20 +2,24 @@ import React, { useCallback } from "react";
 import { DxcTextInput } from "@dxc-technology/halstack-react";
 import { HalApiCaller } from "@dxc-technology/halstack-client";
 
+const addFilterParams = (url, value, propertyName) =>
+  `${url}${url.includes("?") ? "&" : "?"}${propertyName}=${value}`;
+
 const HalAutocomplete = ({ propertyName, url, asyncHeadersHandler, headers, ...childProps }) => {
-  const getSuggestionsFromAPI = useCallback(async () => {
-    const asyncHeadears = asyncHeadersHandler ? await asyncHeadersHandler() : {};
-    const response = await HalApiCaller.get({
-      url: url,
-      headers: { ...headers, ...asyncHeadears },
-    });
-    return response?.halResource?.getItems()
-      ? response.halResource
-          .getItems()
-          .filter((item) => item?.summary?.[propertyName])
-          .map((item) => item.summary[propertyName])
-      : [];
-  }, [propertyName, url, asyncHeadersHandler, headers]);
+  const getSuggestionsFromAPI = useCallback(
+    async (newValue) => {
+      const asyncHeadears = asyncHeadersHandler ? await asyncHeadersHandler() : {};
+      const response = await HalApiCaller.get({
+        url: newValue ? addFilterParams(url, newValue, propertyName) : url,
+        headers: { ...headers, ...asyncHeadears },
+      });
+
+      return response?.halResource?.getItems()
+        ? response.halResource.getItems().map((item) => item.summary[propertyName])
+        : [];
+    },
+    [propertyName, url, asyncHeadersHandler, headers]
+  );
 
   return <DxcTextInput {...childProps} suggestions={getSuggestionsFromAPI} />;
 };
