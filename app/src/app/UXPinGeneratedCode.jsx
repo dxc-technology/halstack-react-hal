6dfce1956b5b8893f9a1abe8f92d62b44c817772
Singh <diwakar.singh2@dxc.com>
2024-02-20 10:47:25 +0100

    HalForm displays API interation errors

--- app/src/app/UXPinGeneratedCode.jsx
+++ app/src/app/UXPinGeneratedCode.jsx
@@ -1,7 +1,7 @@
 /**
  * Info:
  * 1. 'import' statements
- * 2. Component Definition 
+ * 2. Component Definition
  *  are added manually to the generated code
  * */
 
@@ -12,82 +12,64 @@ import {
   DxcRadioGroup,
   DxcSelect,
   DxcTextInput,
-  DxcTextarea
+  DxcTextarea,
 } from "@dxc-technology/halstack-react";
 
 import { HalForm } from "@dxc-technology/halstack-react-hal";
 
-const authHeaders = {
-  "x-auth-username": "DSINGH",
-  "x-api-key": "48SmqcLpec3t1TO8EMzaDaamMz25pDZ469NFux41",
-};
+const authHeaders = {};
 
-const apiEndpoint =
-  "https://diaas-dev.gtaia-test-domain.net/std-dev-lux-alt-13111/insurance/persons/ID-wJsQCHTKS";
+const apiEndpoint = "";
 
 const UXPinGeneratedCode = () => (
-
-
-    <DxcFlex
-      gap="3rem"
-      direction="column"
-    >
-      <HalForm
-        apiEndpoint={apiEndpoint}
-        authHeaders={authHeaders}
-        selfManagedSave={true}
-      >
-        <DxcGrid
-          templateColumns={['repeat(3, 1fr)']}
-          gap="1rem"
-        >
-          <DxcRadioGroup
-            label="Title"
-            options={[
-              { label: 'Female', value: 'female' },
-              { label: 'Male', value: 'male' },
-              { label: 'Non-binary', value: 'non-binary' },
-              { label: 'Other', value: 'other' }
-            ]}
-            name="person:person_title"
-            stacking="row"
-            helperText="Must match official identification documents"
-          />
-          <DxcTextInput
-            label="Family Name"
-            helperText="Must match official identification documents"
-            placeholder="Enter family name"
-            name="person:last_name"
-          />
-          <DxcTextInput
-            label="First Name"
-            helperText="Must match official identification documents"
-            placeholder="Enter first name"
-            name="person:first_name"
-          />
-          <DxcSelect
-            label="Professional  Status"
-            placeholder="Choose a title"
-            name="person:professional_status"
-            helperText="Prefix to Insured's name"
-          />
-          <DxcDateInput
-            label="Date of Birth"
-            helperText="Legal age for insured is above 10 years"
-            placeholder={true}
-            name="person:birth_date"
-            
-          />
-          <DxcTextarea
-            label="Insured Display ID"
-            helperText="This is how Insured ID will appear on documents"
-            placeholder=""
-            name="person:display_id"
-          />
-        </DxcGrid>
-      </HalForm>
-    </DxcFlex>
- 
+  <DxcFlex gap="3rem" direction="column">
+    <HalForm apiEndpoint={apiEndpoint} authHeaders={authHeaders} selfManagedSave={true}>
+      <DxcGrid templateColumns={["repeat(3, 1fr)"]} gap="1rem">
+        <DxcRadioGroup
+          label="Title"
+          options={[
+            { label: "Female", value: "female" },
+            { label: "Male", value: "male" },
+            { label: "Non-binary", value: "non-binary" },
+            { label: "Other", value: "other" },
+          ]}
+          name="person:person_title"
+          stacking="row"
+          helperText="Must match official identification documents"
+        />
+        <DxcTextInput
+          label="Family Name"
+          helperText="Must match official identification documents"
+          placeholder="Enter family name"
+          name="person:last_name"
+        />
+        <DxcTextInput
+          label="First Name"
+          helperText="Must match official identification documents"
+          placeholder="Enter first name"
+          name="person:first_name"
+        />
+        <DxcSelect
+          label="Professional  Status"
+          placeholder="Choose a title"
+          name="person:professional_status"
+          helperText="Prefix to Insured's name"
+        />
+        <DxcDateInput
+          label="Date of Birth"
+          helperText="Legal age for insured is above 10 years"
+          placeholder={true}
+          name="person:birth_date"
+        />
+        <DxcTextarea
+          label="Insured Display ID"
+          helperText="This is how Insured ID will appear on documents"
+          placeholder=""
+          name="person:display_id"
+        />
+      </DxcGrid>
+    </HalForm>
+  </DxcFlex>
 );
 
 export default UXPinGeneratedCode;
